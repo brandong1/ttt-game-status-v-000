8d05aa6cee818987d91b57a84cbff63940896a11
Brandon Green <brandon@brandonleeproductions.com>
2018-05-12 21:59:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,12 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [ 0, 1, 2 ], # Top throw
+  [ 3, 4, 5 ], # Middle row 
+  [ 7, 8, 9 ], # Bottom row
+  
+  
+  
+  ]
