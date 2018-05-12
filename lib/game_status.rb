c2f4ccfc8b7c18cd17d4f5523ee7e74e7d778b23
Brandon Green <brandon@brandonleeproductions.com>
2018-05-12 22:03:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,14 +6,13 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-  [ 0, 1, 2 ], # Top throw
-  [ 3, 4, 5 ], # Middle row 
-  [ 6, 7, 8 ], # Bottom row
-  [ 0, 3, 6 ]
-  [ 1, 4, 7 ]
-  [ 2, 5, 8 ]
-  [ 0, 4, 8 ]
-  [ 6, 4, 2 ]
-  
+  [0, 1, 2 ], # Top throw
+  [3, 4, 5 ], # Middle row 
+  [6, 7, 8 ], # Bottom row
+  [0, 3, 6 ]
+  [1, 4, 7 ]
+  [2, 5, 8 ]
+  [0, 4, 8 ]
+  [6, 4, 2 ]
   
   ]
