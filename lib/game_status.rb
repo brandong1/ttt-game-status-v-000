d9e499872a144aaaf127af25600b7502d9a468a8
Brandon Green <brandon@brandonleeproductions.com>
2018-05-12 22:23:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,6 +30,13 @@ def won?(board)
     win_index_9 = win_combination[8]
     
     position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
     
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else
+       false 
+    end 
   end
 end
