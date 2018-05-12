b2ee4964c2c44b14aaab26094fca5d25dd9c569d
Brandon Green <brandon@brandonleeproductions.com>
2018-05-12 22:05:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-def WIN_COMBINATIONS = [
+WIN_COMBINATIONS = [
   [0, 1, 2], # Top throw
   [3, 4, 5], # Middle row 
   [6, 7, 8], # Bottom row
@@ -16,4 +16,3 @@ def WIN_COMBINATIONS = [
   [6, 4, 2]
   
 ]
-end
